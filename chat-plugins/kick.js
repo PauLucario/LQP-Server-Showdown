1481da3147e944619ce835c0ff6ee91ea4100d77
PauLucario <da.pa.perso@gmail.com>
2015-08-18 00:07:10 +0200

    Update kick.js

--- chat-plugins/kick.js
+++ chat-plugins/kick.js
@@ -6,15 +6,15 @@ exports.commands = {
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		if (!targetUser || !targetUser.connected) {
-			return this.sendReply("User " + this.targetUsername + " not found.");
+			return this.sendReply("Utilisateur " + this.targetUsername + " non trouvé.");
 		}
 		if (!room.users[targetUser.userid]) {
-			return this.sendReply("User " + this.targetUsername + " is not in the room " + room.id + ".");
+			return this.sendReply("L'utilisateur " + this.targetUsername + " n'est pas dans la salle " + room.id + ".");
 		}
 		if (!this.can('kick', targetUser, room)) return false;
-		var msg = "kicked from room " + room.id + " by " + user.name + (target ? " (" + target + ")" : "") + ".";
-		this.addModCommand("" + targetUser.name + " was " + msg);
-		targetUser.popup("You have been " + msg);
+		var msg = "éjecté de la salle " + room.id + " par " + user.name + (target ? " (" + target + ")" : "") + ".";
+		this.addModCommand("" + targetUser.name + " a été " + msg);
+		targetUser.popup("Tu as été " + msg);
 		targetUser.leaveRoom(room);
 	}
 };
